6f86cfe42b2bcd2ea3d36b655ee47ee92f9a491e
Josh Cough <joshcough@gmail.com>
2019-09-04 10:22:26 -0400

    minor fixes

--- frontend/src/Main.purs
+++ frontend/src/Main.purs
@@ -6,7 +6,7 @@ import Effect (Effect)
 import Effect.Console (log)
 import Elmish as Elmish
 import Elmish.Component (ComponentDef)
-import Elmish.Dispatch (dispatchMsgFn)
+import Elmish.Dispatch (DispatchMsgFn, dispatchMsgFn)
 import Elmish.React (ReactElement)
 import Frame as Frame
 import Commands as Commands
@@ -15,12 +15,15 @@ import Types (OpM, runOpM)
 
 main :: Effect Unit
 main = do
-    commands <- go "Commands" Commands.def
-    quotes   <- go "Quotes"  Quotes.def
-    Elmish.boot { domElementId: "app", def: Frame.frame [commands, quotes] }
+  commands <- go "Commands" Commands.def
+  quotes   <- go "Quotes"  Quotes.def
+  Elmish.boot { domElementId: "app", def: Frame.frame [commands, quotes] }
 
-go :: forall msg state. String -> ComponentDef OpM msg state -> Effect { title :: String, view :: ReactElement }
-go title def = do
-  render <- Elmish.construct (Elmish.nat runOpM def)
-  pure { title: title, view: render onError }
-  where onError = dispatchMsgFn log (const $ pure unit)
+  where
+  go :: forall msg state. String -> ComponentDef OpM msg state -> Effect { title :: String, view :: ReactElement }
+  go title def = do
+    render <- Elmish.construct (Elmish.nat runOpM def)
+    pure { title: title, view: render onError }
+
+  onError :: DispatchMsgFn Unit
+  onError = dispatchMsgFn log (const $ pure unit)
