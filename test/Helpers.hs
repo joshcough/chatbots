c4be1843b506e6bdc23d4d9defa5a882cba9b20c
Josh Cough <joshcough@gmail.com>
2019-09-05 22:45:31 -0400

    just some refactorings.

--- test/Helpers.hs
+++ test/Helpers.hs
@@ -8,38 +8,60 @@ import Data.String.Conversions (cs)
 import Data.Text (pack)
 import Database.Persist.Sql (rawExecute)
 import Database.PostgreSQL.Simple.Options (Options(..))
-import Database.Postgres.Temp (DB(..), startLocalhost, stop, defaultOptions)
+import Database.Postgres.Temp (DB(..), defaultOptions)
+import qualified Database.Postgres.Temp as PG
+import Settings (lookupReadableSetting)
+import System.IO (IOMode(WriteMode), openFile)
 import Test.Hspec
-import Turtle.Prelude
+import Turtle.Prelude hiding (stderr, stdout)
 import Turtle.Shell
 import Types (runAppToIO, runDb)
+
+--
+-- NOTE: if having trouble with db, do this: DBLOGGING=VERBOSE stack test
+--
 
 ---
 --- Setup and teardown helpers
 ---
+data DBLogging = VERBOSE | SILENT deriving Read
 
-createDatabase :: IO (DB, Config)
-createDatabase = do
-    Right db@DB{..} <- startLocalhost defaultOptions
-    let connStr = toConnectionString options
-    sh $ do export "DBM_DATABASE" connStr
+withDB :: SpecWith ((DB, IO ()), Config) -> Spec
+withDB = beforeAll createDatabase . afterAll (snd . fst) . after (truncateDb . snd)
+  where
+    createDatabase :: IO ((DB, IO ()), Config)
+    createDatabase = do
+      verbosity <- lookupReadableSetting "DBLOGGING" SILENT
+      (db, io) <- startDb verbosity
+      config <- acquireConfigWithConnStr $ toConnectionString db
+      return ((db, io), config)
+    
+    -- https://stackoverflow.com/questions/5342440/reset-auto-increment-counter-in-postgres
+    truncateDb :: Config -> IO ()
+    truncateDb config = runAppToIO config . runDb $ truncateTables
+      where
+      tables = ["commands", "quotes"]
+      truncateStatement = "TRUNCATE TABLE " <> intercalate ", " tables <> " RESTART IDENTITY CASCADE"
+      truncateTables = rawExecute (pack truncateStatement) []
+    
+    toConnectionString :: DB -> Text
+    toConnectionString DB {..} = "postgresql://" <> user <> "@" <> host <> ":" <> show port <> "/" <> cs oDbname
+      where
+        Options {..} = options
+        host = cs $ fromMaybe "localhost" oHost
+        user = cs $ fromMaybe "josh" oUser
+    
+    startDb :: DBLogging -> IO (DB, IO ())
+    startDb verbosity = mask $ \restore -> do
+      (outHandle, errHandle) <- case verbosity of
+        VERBOSE -> pure (stdout, stderr)
+        SILENT -> (,) <$> devNull <*> devNull
+      db <- PG.startWithHandles PG.Localhost defaultOptions outHandle errHandle >>= either throwIO pure
+      restore (setupDB db >> pure (db, cleanup db)) `onException` cleanup db
+      where
+        devNull = openFile "/dev/null" WriteMode
+        setupDB db =
+          sh $ do
+            export "DBM_DATABASE" $ toConnectionString db
             proc "moo-postgresql" ["upgrade"] empty
-    config <- acquireConfigWithConnStr connStr
-    return (db, config)
-
--- https://stackoverflow.com/questions/5342440/reset-auto-increment-counter-in-postgres
-truncateDb :: Config -> IO ()
-truncateDb config = runAppToIO config . runDb $ truncateTables
-    where
-    tables = ["commands", "quotes"]
-    truncateTables = rawExecute (pack $ "TRUNCATE TABLE " ++ intercalate ", " tables ++ " RESTART IDENTITY CASCADE") []
-
-toConnectionString :: Options -> Text
-toConnectionString Options {..} = "postgresql://" <> user <> "@" <> host <> ":" <> port <> "/" <> cs oDbname
-    where
-    host = cs $ fromMaybe "localhost" oHost
-    user = cs $ fromMaybe "josh" oUser
-    port = show $ fromMaybe 5432 oPort
-
-withDB :: SpecWith (DB, Config) -> Spec
-withDB = beforeAll createDatabase . afterAll (void . stop . fst) . after (truncateDb . snd)
+        cleanup = void . PG.stop
