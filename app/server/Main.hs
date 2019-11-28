781cb2a27794649075cd39b6f9481e1298ac0419
Joshua Cough <joshcough@gmail.com>
2019-11-28 13:20:05 -0500

    test

--- app/server/Main.hs
+++ app/server/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
-import           Init    (runAppAndBot)
-import           Prelude (IO)
+import GHC.IO.Encoding
+import Init (runAppAndBot)
+import Prelude (IO)
 
 -- | The 'main' function gathers the required environment information and
 -- initializes the application.
 main :: IO ()
-main = runAppAndBot
+main = do
+  setLocaleEncoding utf8
+  runAppAndBot
