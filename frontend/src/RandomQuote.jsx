a9021b051402a6ac41d3e6e114cdebeb124b64b1
Josh Cough <joshcough@gmail.com>
2020-06-05 11:56:59 -0400

    made page behave nicer.

--- frontend/src/RandomQuote.jsx
+++ frontend/src/RandomQuote.jsx
@@ -2,5 +2,9 @@ import React from "react"
 import { Dropdown, Table } from "react-bootstrap"
 
 export const View = props =>
-  <div>{props.quote ? <div>Quote #{props.quote.qid}: {props.quote.body}</div> : <div></div> }
+  <div>
+    {props.quote ?
+      <div>Quote #{props.quote.qid}: {props.quote.body}</div> :
+      <div></div>
+    }
   </div>
