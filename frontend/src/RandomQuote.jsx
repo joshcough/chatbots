d0e27840422b91da2d0c3b5c034edc3ad1548a4d
Josh Cough <joshcough@gmail.com>
2020-06-05 10:23:08 -0400

    fix random quote page i hope

--- frontend/src/RandomQuote.jsx
+++ frontend/src/RandomQuote.jsx
@@ -1,4 +1,5 @@
 import React from "react"
 import { Dropdown, Table } from "react-bootstrap"
 
-export const View = props => <div>Quote #{q.qid}: {q.body}</div>
+export const View = props =>
+  {props.quote ? <div>Quote #{props.quote.qid}: {props.quote.body}</div> : ""}
